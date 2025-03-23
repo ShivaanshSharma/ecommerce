fdab774645858ceab1dc882ae90a900b71e0b43b
Shivaansh Sharma <shivaanshsharma2001@gmail.com>
2025-03-24 00:12:36 +0530

    hosting

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,7 +5,7 @@ export default function ErrorPage() {
     return (
         <div className="w-100% p-6 bg-gray-200 text-center">
             <h1 className="text-2xl mb-3">Page not found!</h1>
-            <Link to="/" className="duration-150 bg-red-900 rounded p-2 text-xs text-white hover:bg-white hover:text-red-900">Home</Link>
+            <Link to="/ecommerce" className="duration-150 bg-red-900 rounded p-2 text-xs text-white hover:bg-white hover:text-red-900">Home</Link>
         </div>
     )
 }
