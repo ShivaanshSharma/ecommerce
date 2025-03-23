f221919139cf32c69bb03a0d04f7ab24e85fcfab
Shivaansh Sharma <shivaanshsharma2001@gmail.com>
2025-03-23 23:55:09 +0530

    working on the hosting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
 
 import logo from '../assets/newlogo-bg.png';
 import { CiShoppingCart } from "react-icons/ci";
-import { createPortal } from 'react-dom';
-import { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Header () {
@@ -13,7 +11,7 @@ export default function Header () {
             <img src={logo} className='h-15 w-15'/>
             <span className='text-xl mx-2 font-light'>ShopSphere</span>
             </div>
-            <span className='cursor-pointer bg-red-900 mx-4 text-white p-1 rounded hover:bg-white hover:text-red-600 text-2xl duration-300'><Link to='/Cart'><CiShoppingCart /></Link></span>
+            <span className='cursor-pointer bg-red-900 mx-4 text-white p-1 rounded hover:bg-white hover:text-red-600 text-2xl duration-300'><Link to='/ecommerce/Cart'><CiShoppingCart /></Link></span>
         </div>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,10 +5,10 @@ export default function NavBar () {
     return (
         <div>
             <ul className="flex items-center bg-red-900 text-white text-xs shadow-md gap-3">
-                <li className="hover:text-red-900 hover:bg-white duration-150 p-3 ml-3"><NavLink className={({isActive}) => isActive ? 'underline' : null} to="/" end>Home</NavLink></li>
-                <li className="duration-150 p-3 hover:text-red-900 hover:bg-white"><NavLink className={({isActive}) => isActive ? 'underline' : null} to="/products">Products</NavLink></li>
-                <li className="duration-150 p-3 hover:text-red-900 hover:bg-white"><NavLink className={({isActive}) => isActive ? 'underline' : null} to="/contact">Contact Us</NavLink></li>
-                <li className="duration-150 p-3 hover:text-red-900 hover:bg-white"><NavLink className={({isActive}) => isActive ? 'underline' : null} to="/about">About Us</NavLink></li>
+                <li className="hover:text-red-900 hover:bg-white duration-150 p-3 ml-3"><NavLink className={({isActive}) => isActive ? 'underline' : null} to="/ecommerce" end>Home</NavLink></li>
+                <li className="duration-150 p-3 hover:text-red-900 hover:bg-white"><NavLink className={({isActive}) => isActive ? 'underline' : null} to="/ecommerce/products">Products</NavLink></li>
+                <li className="duration-150 p-3 hover:text-red-900 hover:bg-white"><NavLink className={({isActive}) => isActive ? 'underline' : null} to="/ecommerce/contact">Contact Us</NavLink></li>
+                <li className="duration-150 p-3 hover:text-red-900 hover:bg-white"><NavLink className={({isActive}) => isActive ? 'underline' : null} to="/ecommerce/about">About Us</NavLink></li>
             </ul>
         </div>
     )
